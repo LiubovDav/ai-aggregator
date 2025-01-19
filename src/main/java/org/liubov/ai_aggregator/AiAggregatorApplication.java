95f3d0057b91227ac6836a2acb0bc0a5f3713272
Liubov Dav <lubasha0512@gmail.com>
2025-01-18 21:30:04 -0800

    Initial commit

---
+++ src/main/java/org/liubov/ai_aggregator/AiAggregatorApplication.java
@@ -0,0 +1,13 @@
+package org.liubov.ai_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/liubov/ai_aggregator/AiAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liubov.ai_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
