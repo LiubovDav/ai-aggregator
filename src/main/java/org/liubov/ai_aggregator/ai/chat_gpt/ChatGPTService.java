7aea7de3b36614c25f364fd8527f82cdf5bf0962
Liubov Dav <lubasha0512@gmail.com>
2025-01-26 23:58:08 -0800

    Improved ChatGPT service call

--- src/main/java/org/liubov/ai_aggregator/ai/chat_gpt/ChatGPTService.java
+++ src/main/java/org/liubov/ai_aggregator/ai/chat_gpt/ChatGPTService.java
@@ -1,31 +1,21 @@
 package org.liubov.ai_aggregator.ai.chat_gpt;
 
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatGPTService {
 
-    @Value("${CHAT_GPT_API_KEY}")
-    private String chatGptApiKey;
+    private final OpenAiChatModel chatModel;
+
+    public ChatGPTService(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
 
     public ChatResponse send(String text) {
-        if (chatGptApiKey == null) {
-            System.out.println("***************************************************************************************");
-            System.out.println("chatGptApiKey is null");
-            System.out.println("***************************************************************************************");
-            return null;
-        }
-
-        // todo: initialize ones
-        ChatModel chatModel = new OpenAiChatModel(new OpenAiApi(chatGptApiKey));
-
         ChatResponse response = chatModel.call(
                 new Prompt(
                         text,
